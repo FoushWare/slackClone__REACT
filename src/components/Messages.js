53fa774af77f47c242e935d179afba30f22f7285
foushware <mr.fouad992@gmail.com>
2020-05-02 02:52:01 +0200

    add layout for messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -4,4 +4,6 @@ import styled from 'styled-components';
 export default styled.div`
   grid-column: 3;
   grid-row: 2;
+  padding-left: 20px;
+  padding-right: 20px;
 `;
--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -2,10 +2,33 @@ import React from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 import Messages from '../components/Messages';
+import { Comment } from 'semantic-ui-react';
+
 
 const MessageContainer = ({data:{loading, messages}}) => 
 
-(loading?null:<Messages>{JSON.stringify(messages)}</Messages>);
+(loading?null:(
+
+    
+    <Messages>
+      <Comment.Group>
+        {messages.map(m => (
+          <Comment key={`${m.id}-message`}>
+            <Comment.Content>
+              <Comment.Author as="a">{m.user.username}</Comment.Author>
+              <Comment.Metadata>
+                <div>{m.createdAt}</div>
+              </Comment.Metadata>
+              <Comment.Text>{m.text}</Comment.Text>
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        ))}
+      </Comment.Group>
+    </Messages>
+));
 const  messagesQuery = gql`
 
     query($channelId: Int! ){
@@ -16,6 +39,7 @@ const  messagesQuery = gql`
                 username
             }
             createdAt
+            
         }
     }
 
